72dc45d88a7ea404953aba5e32b818c0faac5efe
Gregory <gregory.facyt@gmail.com>
2018-05-11 17:03:04 -0400

    integracion con materializecss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-// eslint-disable-next-line
-
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+
+import {Button, Icon, SideNav, SideNavItem} from 'react-materialize'
 //import logo from './logo.svg';
 
 class App extends Component {
@@ -10,19 +10,42 @@ class App extends Component {
 
 		const { user } = this.props.data;
 
-		console.log(this.props.data);
+		console.log('user', user);
+
+		const obj = {
+	  		background: 'https://pre00.deviantart.net/ef23/th/pre/i/2017/294/2/9/goku_miggate_no_goku_i_3_584_by_koku78-dbrb1j0.png',
+	  		image: 'https://pre00.deviantart.net/ef23/th/pre/i/2017/294/2/9/goku_miggate_no_goku_i_3_584_by_koku78-dbrb1j0.png',
+	  		name: 'HOLA'
+	  	}
+
+	  	console.log('OBJ',obj);
+
 		return (
 			<div key={1}>
-				 {this.props.data.loading &&
-				 	<p>LOADING</p>
-				 }
-
-				 {!this.props.data.loading &&
-					<div>
-						<h1>{ user.name }</h1>
-						<h2>{ user.email }</h2>
-					</div>	
-				 }
+				 
+				{!this.props.data.loading &&
+				  	<SideNav
+				  		trigger={<Button>SIDE NAV DEMO</Button>}
+				  		options={{ closeOnClick: true }}
+				  	>
+				  	<SideNavItem userView
+					  	user={{
+					  		background: 'https://pre00.deviantart.net/ef23/th/pre/i/2017/294/2/9/goku_miggate_no_goku_i_3_584_by_koku78-dbrb1j0.png',
+					  		image: 'https://pre00.deviantart.net/ef23/th/pre/i/2017/294/2/9/goku_miggate_no_goku_i_3_584_by_koku78-dbrb1j0.png',
+					  		name: `${user.name}`,
+					  		email: `${user.email}`
+					  	}}
+				  	/>
+				  	<SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
+				  	<SideNavItem href='#!second'>Second Link</SideNavItem>
+				  	<SideNavItem divider />
+				  	<SideNavItem subheader>Subheader</SideNavItem>
+				  	<SideNavItem divider />
+				  	<SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>
+				  	<SideNavItem divider />
+				  	</SideNav>
+					
+				}
 			</div>
 		)
 	}
