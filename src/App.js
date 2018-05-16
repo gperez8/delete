10d54e443a49de23be816cbab3844d4db0ad220e
gperez8 <gregory.facyt@gmail.com>
2018-05-16 05:47:14 -0400

    drawer

--- src/App.js
+++ src/App.js
@@ -1,63 +1,163 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
-import {Button, Icon, SideNav, SideNavItem} from 'react-materialize'
-//import logo from './logo.svg';
+const drawerWidth = 240;
 
-class App extends Component {
-	render() {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 'auto',
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing.unit * 9,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
 
-		const { user } = this.props.data;
+class MiniDrawer extends React.Component {
+  state = {
+    open: false,
+  };
 
-		console.log('user', user);
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
 
-		const obj = {
-	  		background: 'https://pre00.deviantart.net/ef23/th/pre/i/2017/294/2/9/goku_miggate_no_goku_i_3_584_by_koku78-dbrb1j0.png',
-	  		image: 'https://pre00.deviantart.net/ef23/th/pre/i/2017/294/2/9/goku_miggate_no_goku_i_3_584_by_koku78-dbrb1j0.png',
-	  		name: 'HOLA'
-	  	}
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
 
-	  	console.log('OBJ',obj);
+  render() {
+    const { classes, theme } = this.props;
 
-		return (
-			<div key={1}>
-				 
-				{!this.props.data.loading &&
-				  	<SideNav
-				  		trigger={<Button>SIDE NAV DEMO</Button>}
-				  		options={{ closeOnClick: true }}
-				  	>
-				  	<SideNavItem userView
-					  	user={{
-					  		background: 'https://pre00.deviantart.net/ef23/th/pre/i/2017/294/2/9/goku_miggate_no_goku_i_3_584_by_koku78-dbrb1j0.png',
-					  		image: 'https://pre00.deviantart.net/ef23/th/pre/i/2017/294/2/9/goku_miggate_no_goku_i_3_584_by_koku78-dbrb1j0.png',
-					  		name: `${user.name}`,
-					  		email: `${user.email}`
-					  	}}
-				  	/>
-				  	<SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
-				  	<SideNavItem href='#!second'>Second Link</SideNavItem>
-				  	<SideNavItem divider />
-				  	<SideNavItem subheader>Subheader</SideNavItem>
-				  	<SideNavItem divider />
-				  	<SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>
-				  	<SideNavItem divider />
-				  	</SideNav>
-					
-				}
-			</div>
-		)
-	}
+    return (
+      <div className={classes.root}>
+        <AppBar
+          position="absolute"
+          className={classNames(classes.appBar, this.state.open && classes.appBarShift)}
+        >
+          <Toolbar disableGutters={!this.state.open}>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerOpen}
+              className={classNames(classes.menuButton, this.state.open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" noWrap>
+              Mini variant drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+          }}
+          open={this.state.open}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={this.handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+
+          <Divider />
+
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+          <h1>Contenido</h1>
+        </main>
+      </div>
+    );
+  }
 }
 
-const user = gql`
+MiniDrawer.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(MiniDrawer);
+
+
+/*const user = gql`
 	query {
 		user(id: 1) {
 			name
 			email
 		}
 	}
-`
+`*/
 
-export default graphql(user)(App);
+//export default graphql(user)(App);
